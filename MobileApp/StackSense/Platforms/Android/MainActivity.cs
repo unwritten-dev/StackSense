89d30003d43d28f1d5642bb62f08e76cefcc95b2
Nils Krautkraemer <nils@krautkraemer.dev>
2026-02-14 22:20:48 +0100

    Initial Blazor Setup

---
+++ MobileApp/StackSense/Platforms/Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
+
+namespace StackSense;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Make top bar invisible
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+        {
+            Window?.SetDecorFitsSystemWindows(false);
+        }
+        else if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+        {
+            Window?.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            Window?.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+        }
+
+        // Make icons of top bar white
+        if (Window != null)
+        {
+            var windowInsetsController = WindowCompat.GetInsetsController(Window, Window.DecorView);
+            if (windowInsetsController != null)
+            {
+                windowInsetsController.AppearanceLightStatusBars = false;
+            }
+        }
+    }
+}
---
+++ MobileApp/StackSense/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StackSense
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
